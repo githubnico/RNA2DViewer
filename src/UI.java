3baba4e574ee36c2a1648efbda1e863e60a920f4
testapp1 <testapp1@web.de>
2015-12-01 15:39:44 +0100

    drag and drop implemented

--- src/UI.java
+++ src/UI.java
@@ -1,22 +1,24 @@
+import javafx.animation.TranslateTransition;
+import javafx.animation.TranslateTransitionBuilder;
 import javafx.application.Application;
+import javafx.beans.value.ObservableBooleanValue;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.stage.Stage;
-import javafx.util.Pair;
-import javafx.scene.Group;
-import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.ArcType;
-import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -25,6 +27,18 @@ import java.util.ArrayList;
  * Created by Deviltech on 01.12.2015.
  */
 public class UI extends Application{
+
+    double orgSceneX;
+    double orgSceneY;
+    double orgTranslateX;
+    double orgTranslateY;
+    boolean isAnimated;
+
+    Pane drawPane;
+    ArrayList<Circle> circleList = new ArrayList<>();
+
+    Graph myGraph;
+    final double[][][] coordsRepresentation = {new double[1][2]};
 
 
     @Override
@@ -37,20 +51,30 @@ public class UI extends Application{
         Button computeButton = new Button(myLabels.BUTTON_COMPUTE);
         Button drawButton = new Button(myLabels.BUTTON_DRAW);
         CheckBox animateChecker = new CheckBox(myLabels.CHECKBOX_ANIMATE);
-        Pane drawPane = new Pane();
+        drawPane = new Pane();
 
         Canvas canvas = new Canvas(400, 400);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
 
         buttonBox.getChildren().addAll(computeButton, drawButton, animateChecker);
         mainBox.getChildren().addAll(sequenceField, bracketField, buttonBox, drawPane);
+
+        animateChecker.setOnAction((value) ->{
+            if (animateChecker.isSelected() != isAnimated){
+                isAnimated = animateChecker.isSelected();
+                for(Circle currentCircle: circleList){
+                    currentCircle.setOnMousePressed(isAnimated ?  circleOnMousePressedEventHandler : null);
+                    currentCircle.setOnMouseDragged(isAnimated ?  circleOnMouseDraggedEventHandler : null);
+                    currentCircle.setOnMouseReleased(isAnimated ?  circleOnMouseReleasedEventHandler : null);
+                }
+
+            }
+        });
 
         // computeButton
         computeButton.setOnAction((value) -> {
             bracketField.setText(new Nussinov(sequenceField.getText()).getBracketNotation());
         });
 
-        final double[][][] coordsRepresentation = {new double[1][2]};
 
         // drawButton
         drawButton.setOnAction((value) -> {
@@ -60,7 +84,7 @@ public class UI extends Application{
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            coordsRepresentation[0] = SpringEmbedder.computeSpringEmbedding(50, myGraph.getNumberOfNodes(), myGraph.getEdges(), null );
+            coordsRepresentation[0] = SpringEmbedder.computeSpringEmbedding(100, myGraph.getNumberOfNodes(), myGraph.getEdges(), null );
             SpringEmbedder.centerCoordinates(coordsRepresentation[0], 10, 400, 10, 400);
             drawShapes(drawPane, coordsRepresentation[0], myGraph.getEdges(), myGraph.getNumberOfNodes());
         });
@@ -83,10 +107,21 @@ public class UI extends Application{
 
     private void drawShapes(Pane drawPane, double[][] coords, int[][] edges, int startIndex) {
         drawPane.getChildren().clear();
-        ArrayList<Circle> circleList = new ArrayList<>();
+        circleList = new ArrayList<>();
+
         // generate Circles
-        for (double[] currentCoord: coords){
-            Circle currentCircle = new Circle(currentCoord[0],currentCoord[1], 5, Color.BLACK);
+        for (int i = 0; i < coords.length; i++){
+            Circle currentCircle = new Circle(coords[i][0],coords[i][1], 5, Color.BLACK);
+            Tooltip.install(
+                    currentCircle,
+                    new Tooltip(Integer.toString(i+1))
+            );
+            if(isAnimated){
+                currentCircle.setOnMousePressed(circleOnMousePressedEventHandler);
+                currentCircle.setOnMouseDragged(circleOnMouseDraggedEventHandler);
+                currentCircle.setOnMouseReleased(circleOnMouseReleasedEventHandler);
+            }
+
             circleList.add(currentCircle);
         }
 
@@ -124,7 +159,6 @@ public class UI extends Application{
 
             lineList.add(line);
         }
-        System.out.println(Graph.ArrayToString(edges));
 
 
         drawPane.getChildren().addAll(lineList);
@@ -132,4 +166,45 @@ public class UI extends Application{
 
 
     }
+
+    EventHandler<MouseEvent> circleOnMousePressedEventHandler =
+            new EventHandler<MouseEvent>() {
+
+                @Override
+                public void handle(MouseEvent t) {
+                    orgSceneX = t.getSceneX();
+                    orgSceneY = t.getSceneY();
+                    orgTranslateX = ((Circle)(t.getSource())).getTranslateX();
+                    orgTranslateY = ((Circle)(t.getSource())).getTranslateY();
+                }
+            };
+
+    EventHandler<MouseEvent> circleOnMouseDraggedEventHandler =
+            new EventHandler<MouseEvent>() {
+
+                @Override
+                public void handle(MouseEvent t) {
+                    double offsetX = t.getSceneX() - orgSceneX;
+                    double offsetY = t.getSceneY() - orgSceneY;
+                    double newTranslateX = orgTranslateX + offsetX;
+                    double newTranslateY = orgTranslateY + offsetY;
+
+                    ((Circle)(t.getSource())).setTranslateX(newTranslateX);
+                    ((Circle)(t.getSource())).setTranslateY(newTranslateY);
+                }
+            };
+
+    EventHandler<MouseEvent> circleOnMouseReleasedEventHandler =
+            new EventHandler<MouseEvent>() {
+
+                @Override
+                public void handle(MouseEvent t) {
+
+                    TranslateTransition move = new TranslateTransition(new Duration(200), (Circle) (t.getSource()));
+                    move.setToX(orgTranslateX);
+                    move.setToY(orgTranslateY);
+
+                    move.playFromStart();
+                }
+            };
 }
